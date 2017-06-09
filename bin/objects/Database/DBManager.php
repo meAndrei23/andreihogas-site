b8cd0ff04ca241d1863c010d58b513fbd4ceb305
Pumahawk <Lorenzoga94@gmail.com>
2017-06-10 00:12:42 +0200

    Creata l'interfaccia di collegamento al database
    Signed-off-by: Pumahawk <Lorenzoga94@gmail.com>

---
+++ bin/objects/Database/DBManager.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database;
+
+use Option\Option;
+
+class DBManager{
+	public $pdo;
+	
+	function __costructor(){
+		$opt = new Option();
+		$config = $opt -> readOptions('database');
+		
+		$dsn = $config['type'].':dbname='.$config['database'].';host='.$config['host'];
+		$user = $config['username'];
+		$password = $config['password'];
+
+		try {
+			$dbh = new PDO($dsn, $user, $password);
+			$this -> pdo = $dbh;
+		} catch (PDOException $e) {
+			echo 'Connection failed: ' . $e->getMessage();
+		}
+		$this -> pdo = false;
+	}
+}
